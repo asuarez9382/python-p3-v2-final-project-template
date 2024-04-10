0efb853f24f6d429646d97db646a007c71d0a4a8
Adrian Suarez <asuarez9382@gmail.com>
2024-04-10 09:01:56 -0700

    adds __repr__ function to song class

--- lib/models/song.py
+++ lib/models/song.py
@@ -1,5 +1,18 @@
 from __init__ import CONN, CURSOR
 
 class Song:
-    def __init__(self):
-        pass
+    
+    all = {}
+    
+    def __init__(self, title, genre, duration, artist, id = None, playlistID = None):
+        self.title = title
+        self.genre = genre
+        self.duration = duration
+        self.artist = artist
+        self.playlistID = playlistID
+        self.id = id
+        
+    def __repr__(self):
+        return f'Song ID: {self.id} Song Title: {self.title} Song genre: {self.genre} Duration: {self.duration} Artist: {self.artist}'
+    
+    
